b14115acbef8a6d7681a43f7cb21ce4c2ebb8550
xCloiz <maxime.cloiseau@gmail.com>
2023-11-08 00:50:24 +0100

    Initial Commit

---
+++ StartupMVVMTemplate/StartupMVVMTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartupMVVMTemplateApp.swift
+//  StartupMVVMTemplate
+//
+//  Created by Maxime Parmantier Cloiseau on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct StartupMVVMTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
